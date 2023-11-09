a794aff1394a407bc353475639dfb9bd96ba1cba
Suzana <grdic.suzana@gmail.com>
2023-11-09 11:51:59 +0100

    refactor js

--- main.js
+++ main.js
@@ -1,74 +1,58 @@
 $( document ).ready(function() {
   console.log( "ready!" );
 
+  // global vars
   let myInterval;
   let isActive = false;
+  const end = 100; // hardcoded for now, but can be taken from an input field
+  const speed = 200
+
   const startCounter = (element,end,increment) => {
-    //console.log('pokrenula se');
     isActive = true;
     $('#button').text('Pause')
-    myInterval = setInterval( ()=>{
-     let vrijednost = $('#element').text();
-     vrijednost = parseInt(vrijednost)
-     vrijednost = vrijednost + increment;
-     if (vrijednost % 10 === 0) {
-      $('#element').css({ 'color': 'blue', 'font-size': '300px' });
-     } else {
-      $('#element').css({ 'color': 'skyblue', 'font-size': '200px' });
-     }
-     if (vrijednost === 100) {
-      $('#element').css({ 'color': 'turquoise', 'font-size': '500px' });
-     }
-     console.log(vrijednost)
-     if (vrijednost > end) {
-     pauseCounter();
-     $('#button').text('Reset');
-    } else {
-      $('#element').text(vrijednost);
+    myInterval = setInterval(() => {
+      const currentValue = parseInt(element.text())
+      const nextValue = currentValue + increment
 
-    }
-  }
-      , 2000)
+      if (nextValue % 10 === 0) {
+        element.css({ 'color': 'blue', 'font-size': '300px' });
+      } else {
+        element.css({ 'color': 'skyblue', 'font-size': '200px' });
+      }
+
+      if (nextValue === 100) {
+        element.css({ 'color': 'turquoise', 'font-size': '500px' });
+      }
+      if (nextValue > end) {
+        pauseCounter();
+        $('#button').text('Reset');
+      } else {
+        element.text(nextValue);
+      }
+    }, speed)
   };
   
-
-
   const pauseCounter = () => {
-   isActive = false;
-   clearInterval(myInterval) 
-   $('#button').text('Resume')
-    
-    console.log('pause')
+    isActive = false;
+    clearInterval(myInterval) 
+    $('#button').text('Resume')    
   }
 
-  // promjeni css od countera kada je counter dijeljiv s 10
-  // vrati kako je bilo kad nije
-  const applyCss = () => {
-    // promjeni boju u crvenu
+  const resetCounter = () => {
+    $('#element').text('0');
+    $('#button').text('Start')
   }
   
-  
-  //startCounter ($('#element'), 5, 1);
-  const resetFunction = () => {
-    let vrijednost = $('#element').text('0');
-  }
-  
-  $('#button').on( "click", function() {
-    const end = 100;
-    if (isActive===false) {
-    startCounter ($('#element'), end, 1);
-    const noviBroj = parseInt($('#element').text())
-    if (noviBroj >= end) {
-      resetFunction();
+  $('#button').on("click", function() {
+    if (isActive) {
+      pauseCounter()
+    } else {
+      const currentValue = parseInt($('#element').text())
+      if (currentValue >= end) {
+        resetCounter();
+      } else {
+        startCounter($('#element'), end, 1);
+      }
     }
-  }
-    
-    else {
-    pauseCounter ()
-    }
-    
-  } );
-
-
+  });
 });
-
